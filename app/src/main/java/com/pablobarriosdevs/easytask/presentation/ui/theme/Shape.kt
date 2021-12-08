aecbfc9c52d0e7adc03e3382c86779ebe439f6c1
Pablo Barrios <pablob1518@hotmail.com>
2021-12-08 09:14:14 -0300

    added dependencies

--- app/src/main/java/com/pablobarriosdevs/easytask/presentation/ui/theme/Shape.kt
+++ app/src/main/java/com/pablobarriosdevs/easytask/presentation/ui/theme/Shape.kt
@@ -1,4 +1,4 @@
-package com.pablobarriosdevs.easytask.ui.theme
+package com.pablobarriosdevs.easytask.presentation.ui.theme
 
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Shapes
--- app/src/main/java/com/pablobarriosdevs/easytask/presentation/ui/theme/Theme.kt
+++ app/src/main/java/com/pablobarriosdevs/easytask/presentation/ui/theme/Theme.kt
@@ -1,4 +1,4 @@
-package com.pablobarriosdevs.easytask.ui.theme
+package com.pablobarriosdevs.easytask.presentation.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
